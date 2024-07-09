f6e13ad2de6a3ea55d6e3d021dd4e1aa29d8edfa
Codedwells <wells.mail.13@gmail.com>
2024-07-09 10:56:12 +0300

    feat(plug): Update whitelist

--- src/duckbuck_frontend/src/lib/connect-plug.js
+++ src/duckbuck_frontend/src/lib/connect-plug.js
@@ -1,40 +1,46 @@
 import { toast } from 'sonner'
 
 const PlugConnect = async () => {
-    try {
-        if (
-            typeof window.ic !== 'undefined' &&
-            typeof window.ic.plug !== 'undefined'
-        ) {
-            const plug = window.ic.plug
+	try {
+		if (
+			typeof window.ic !== 'undefined' &&
+			typeof window.ic.plug !== 'undefined'
+		) {
+			const plug = window.ic.plug
 
-            const connected = await plug.requestConnect({
-                whitelist: ['phu3j-cyaaa-aaaam-acr3q-cai']
-            })
+			const connected = await plug.requestConnect({
+				whitelist: [
+					'phu3j-cyaaa-aaaam-acr3q-cai',
+					'y5yya-zqaaa-aaaam-acsea-cai'
+				]
+			})
 
-            if (connected) {
-                toast.success('Plug wallet connected successfully')
-                const principal = await plug.getPrincipal()
+			// Frontend canister ID = y5yya-zqaaa-aaaam-acsea-cai
+			// Token canister ID = phu3j-cyaaa-aaaam-acr3q-cai
 
-                function shortenPrincipal(principalId) {
-                    const principalStr = principalId.toText()
-                    if (principalStr.length <= 6) {
-                        return principalStr // If the ID is too short, return it as is.
-                    }
-                    const start = principalStr.slice(0, 3)
-                    const end = principalStr.slice(-3)
-                    return `${start}...${end}`
-                }
+			if (connected) {
+				toast.success('Plug wallet connected successfully')
+				const principal = await plug.getPrincipal()
 
-                const newPrin = shortenPrincipal(principal)
-                setShow(newPrin)
-            }
-        } else {
-            toast.error('Plug wallet not found')
-        }
-    } catch (error) {
-        console.log('Could not connect to wallet')
-    }
+				function shortenPrincipal(principalId) {
+					const principalStr = principalId.toText()
+					if (principalStr.length <= 6) {
+						return principalStr // If the ID is too short, return it as is.
+					}
+					const start = principalStr.slice(0, 3)
+					const end = principalStr.slice(-3)
+					return `${start}...${end}`
+				}
+
+				const newPrin = shortenPrincipal(principal)
+				setShow(newPrin)
+			}
+		} else {
+			toast.error('Plug wallet not found')
+		}
+	} catch (error) {
+		console.log('Could not connect to wallet')
+	}
 }
 
 export default PlugConnect
